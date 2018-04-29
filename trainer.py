fd8eb590e4f0d735b81d706aa9eb097b6898d27b
Pandu Rendradjaja <prendradjaja@gmail.com>
2018-04-29 09:53:04 -0700

    ++trainer

--- trainer.py
+++ trainer.py
@@ -1,17 +1,49 @@
 import random
+import sys
 
-def main(allowed_letters = 'aoeuidhtns'):
+ALLOWED = ' '.join([
+    'aoeuidhtns',
+    'rl',
+    'cm',
+    'fj',
+    'gp',
+    'wy',
+    'bv',
+    'kx',
+    'qz',
+])
+
+def main(start, allowed_letters = ALLOWED):
     words = get_words()
     writables = (
         [w for w in words
         if only_contains(allowed_letters, w)
-        and len(w) >= 3]
-    )[:100]
+        # and (False
+        #     # or 'r' in w
+        #     # or 'l' in w
+        #     # or 'c' in w
+        #     # or 'm' in w
+        #     # or 'j' in w
+        #     # or 'f' in w
+        #     # or 'g' in w
+        #     # or 'p' in w
+        #     # or 'w' in w
+        #     # or 'y' in w
+        #     # or 'b' in w
+        #     # or 'v' in w
+        #     # or 'k' in w
+        #     # or 'x' in w
+        #     or 'q' in w
+        #     or 'z' in w
+        #     )
+        and 7>= len(w) >= 3]
+    )
+    writables.extend(writables[:150])  # higher probability of seeing common words
     for i in range(100):
         line = []
         for j in range(5):
             line.append(random.choice(writables))
-        print(str(i+1) + ':', ' '.join(line))
+        print(str(i+start) + ':', ' '.join(line))
         try:
             input()
         except (EOFError, KeyboardInterrupt) as e:
@@ -25,4 +57,9 @@ def only_contains(letters, word):
     return all(c in letters for c in word)
 
 if __name__ == '__main__':
-    main()
+    start = 1
+    try:
+        start = int(sys.argv[-1])
+    except:
+        pass
+    main(start)
